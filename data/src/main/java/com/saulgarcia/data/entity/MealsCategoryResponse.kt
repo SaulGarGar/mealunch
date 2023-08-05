cc88a4b969dbf30e1c8240102ca3a1368311f522
saul <saul.garcia@globant.com>
2023-08-05 15:13:12 -0600

    Add models from service.

--- data/src/main/java/com/saulgarcia/data/entity/MealsCategoryResponse.kt
+++ data/src/main/java/com/saulgarcia/data/entity/MealsCategoryResponse.kt
@@ -3,6 +3,12 @@ package com.saulgarcia.data.entity
 import com.google.gson.annotations.SerializedName
 
 data class MealsCategoryResponse(
-    @SerializedName("id")
-    val id: Int? = null
+    @SerializedName("idCategory")
+    val id: Int? = null,
+    @SerializedName("strCategory")
+    val name: String? = null,
+    @SerializedName("strCategoryThumb")
+    val imageUrl: String? = null,
+    @SerializedName("strCategoryDescription")
+    val description: String? = null
 )
--- data/src/main/java/com/saulgarcia/data/mapper/MealsCategoryMapper.kt
+++ data/src/main/java/com/saulgarcia/data/mapper/MealsCategoryMapper.kt
@@ -3,4 +3,9 @@ package com.saulgarcia.data.mapper
 import com.saulgarcia.domain.entity.MealsCategory
 import com.saulgarcia.data.entity.MealsCategoryResponse
 
-fun MealsCategoryResponse.toMealsCategory() = MealsCategory(id = id)
+fun MealsCategoryResponse.toMealsCategory() = MealsCategory(
+    id = id,
+    name = name,
+    imageUrl = imageUrl,
+    description = description
+)
